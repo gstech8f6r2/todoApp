84d558b2f9615e8b4d436df20b39549f391fe80d
gstech8f6r2 <gstech8f6r2@gmail.com>
2020-07-14 19:21:59 +0200

    read function query select * from todo database ciclato in foreach con filtro if

--- application/models/Model.php
+++ application/models/Model.php
@@ -4,4 +4,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Model extends CI_Model {
 
+// CRUD
+
+// C create
+
+// R Read
+
+function read_todos(){
+  $query= $this->db->query('SELECT * FROM todo');
+
+  if ($query->num_rows() > 0) {
+    foreach ($$query->result() as $row) {
+      $data[] = $row;
+    }
+
+    return $data;
+  }
+}
+
+//U Update
+
+// D Delete
+
 }
