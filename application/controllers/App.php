5a80371e16a0cad85364a42b70d6323aa99936a8
gstech8f6r2 <gstech8f6r2@gmail.com>
2020-07-15 11:18:19 +0200

    creiamo controller chiamato App

---
+++ application/controllers/App.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// class App e nome file App.php devono coincidere
+class App extends CI_Controller {
+
+  function index(){
+    echo "Ciao Giacomo!";
+  }
+
+}
